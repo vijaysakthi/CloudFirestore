a7667ab13a3854c2f16260349ae0a1cc8950d7e3
Vijay <svijay3161@gmail.com>
2018-09-11 17:04:33 +0530

    Source Code Firestore

--- app/src/main/java/com/example/vijay/cloudfirestore/MainActivity.java
+++ app/src/main/java/com/example/vijay/cloudfirestore/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//Save Details
+//Save Detail
     private void saveDetails() {
 
         String Name = name_et.getText().toString();
